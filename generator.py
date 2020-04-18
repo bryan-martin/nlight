93efde84e1f6d70567b72639ab7a4982a06db823
Bryan Martin <bmartin2@spacex.corp>
2020-04-18 12:20:09 -0700

    tests now pass

--- generator.py
+++ generator.py
@@ -20,7 +20,7 @@ class BinaryExpression(object):
         self.right = right
 
     def __str__(self):
-        return str(self.left) + " " + self.op + " "  + str(self.right)
+        return str(self.left) + " " + self.op + " " + str(self.right)
 
 
 class ParenthesizedExpression(object):
@@ -55,31 +55,49 @@ def generate_random_expr(prob=1, ops=["+", "-", "*", "/"]):
 
 
 def make_output_file(filename, expr, length, ops=["+", "-", "*", "/"]):
-    with open(filename, 'w') as fd:
+    with open(filename, "w") as fd:
         for _ in range(length):
-            fd.write(str(expr(ops=ops)) + '\n')
+            fd.write(str(expr(ops=ops)) + "\n")
 
 
 def make_test_inputs(ops=["+", "-", "*", "/"]):
     for op in ops:
-        make_output_file('binary_{}_test_input.txt'.format(str(ord(op))), generate_binary_expr, 100, ops=[op])
-        make_output_file('random_{}_test_input.txt'.format(str(ord(op))), generate_random_expr, 100, ops=[op])
-    make_output_file('binary_test_input.txt', generate_binary_expr, 100, ops=ops)
-    make_output_file('random_test_input.txt', generate_random_expr, 100, ops=ops)
+        make_output_file(
+            "binary_{}_test_input.txt".format(str(ord(op))),
+            generate_binary_expr,
+            100,
+            ops=[op],
+        )
+        make_output_file(
+            "random_{}_test_input.txt".format(str(ord(op))),
+            generate_random_expr,
+            100,
+            ops=[op],
+        )
+    make_output_file("binary_test_input.txt", generate_binary_expr, 100, ops=ops)
+    make_output_file("random_test_input.txt", generate_random_expr, 100, ops=ops)
 
 
 def make_truth_file(source_file, filename):
-    with open(source_file, 'r') as rx_fd:
-        with open(filename, 'w') as wx_fd:
+    with open(source_file, "r") as rx_fd:
+        with open(filename, "w") as wx_fd:
             for line in rx_fd:
-                proc = subprocess.run('calc "{}"'.format(line.strip()), shell=True, capture_output=True)
-                output = proc.stdout.decode('ascii').replace("~", "")
+                proc = subprocess.run(
+                    'calc "{}"'.format(line.strip()), shell=True, capture_output=True
+                )
+                output = proc.stdout.decode("ascii").replace("~", "")
                 wx_fd.write(output.strip() + "\n")
 
 
 def make_truth_data(ops=["+", "-", "*", "/"]):
     for op in ops:
-        make_truth_file('binary_{}_test_input.txt'.format(str(ord(op))), 'binary_{}_test_truth.txt'.format(str(ord(op))))
-        make_truth_file('random_{}_test_input.txt'.format(str(ord(op))), 'random_{}_test_truth.txt'.format(str(ord(op))))
-    make_truth_file('binary_test_input.txt', 'binary_test_truth.txt')
-    make_truth_file('random_test_input.txt', 'random_test_truth.txt')
+        make_truth_file(
+            "binary_{}_test_input.txt".format(str(ord(op))),
+            "binary_{}_test_truth.txt".format(str(ord(op))),
+        )
+        make_truth_file(
+            "random_{}_test_input.txt".format(str(ord(op))),
+            "random_{}_test_truth.txt".format(str(ord(op))),
+        )
+    make_truth_file("binary_test_input.txt", "binary_test_truth.txt")
+    make_truth_file("random_test_input.txt", "random_test_truth.txt")
